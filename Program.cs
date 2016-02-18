3462a308c428deeadf59535a8bc4c7a7787a13b7
Silas Siderio <silas.siderio@gmail.com>
2016-02-18 18:32:36 -0200

    Teste Recorrente
    Sequência de Fibonacci

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int r;
+            for (int i = 0; i < n; i++)
+            {
+                r = a;
+                a = b;
+                b = r + b;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
